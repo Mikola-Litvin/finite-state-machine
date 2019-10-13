02dae3b32cd8bb1d235d1d91623304b2375e3cb5
Nikolai Shkrabkov <grr_by@mail.ru>
2019-10-13 12:28:13 +0300

    feat: add code to the methods of class FSM

--- src/fsm.js
+++ src/fsm.js
@@ -3,30 +3,63 @@ class FSM {
      * Creates new FSM instance.
      * @param config
      */
-    constructor(config) {}
+    constructor(config) {
+
+        if (!config) throw new Error("Error");
+
+        this.initial = config.initial;
+        this.state = this.initial;
+        this.states = config.states;
+        this.prevStates = [];
+        this.nextStates = [];
+    }
 
     /**
      * Returns active state.
      * @returns {String}
      */
-    getState() {}
+    getState() {
+
+        return this.state;
+    }
 
     /**
      * Goes to specified state.
      * @param state
      */
-    changeState(state) {}
+    changeState(state) {
+
+        if (!(state in this.states)) throw new Error("Error");
+        
+        this.prevStates.push(this.state);
+        this.nextStates = [];
+        
+        this.state = state;
+    }
 
     /**
      * Changes state according to event transition rules.
      * @param event
      */
-    trigger(event) {}
+    trigger(event) {
+
+        let obj = this.states[this.state]["transitions"];
+
+        if (!(event in obj)) throw new Error("Error");
+
+        this.prevStates.push(this.state);
+        this.nextStates = [];
+        
+        this.state = (obj[event]);   
+    }
 
     /**
      * Resets FSM state to initial.
      */
-    reset() {}
+    reset() {
+
+        this.state = this.initial;
+    }
 
     /**
      * Returns an array of states for which there are specified event transition rules.
@@ -34,26 +67,80 @@ class FSM {
      * @param event
      * @returns {Array}
      */
-    getStates(event) {}
+    getStates(event) {
+
+        let arr = [];
+
+        if (!event) {
+
+            for (let key in this.states) {
+
+                arr.push(key);
+            }
+        }
+        else {
+
+            let arr2 = [], obj;
+
+            for (let key in this.states) {
+
+                arr.push(key);
+            }
+            
+            for (let i = 0; i < arr.length; i++) {
+
+                obj = this.states[arr[i]]["transitions"];
+
+                if (event in obj) {
+
+                    arr2.push(arr[i]);
+                }
+            }
+
+            arr = arr2;
+        }
+
+        return arr;
+    }
 
     /**
      * Goes back to previous state.
      * Returns false if undo is not available.
      * @returns {Boolean}
      */
-    undo() {}
+    undo() {
+    
+        if (this.prevStates.length == 0) return false;
+
+        this.nextStates.push(this.state);
+        this.state = this.prevStates.pop();
+
+        return true;
+    }
 
     /**
      * Goes redo to state.
      * Returns false if redo is not available.
      * @returns {Boolean}
      */
-    redo() {}
+    redo() {
+
+        if (this.nextStates.length == 0) return false;
+
+        this.prevStates.push(this.state);
+        this.state = this.nextStates.pop();
+
+        return true;
+    }
 
     /**
      * Clears transition history
      */
-    clearHistory() {}
+    clearHistory() {
+
+        this.prevStates = [];
+        this.nextStates = [];
+    }
 }
 
 module.exports = FSM;
